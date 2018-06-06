af50a541aa9f49b94a48a87a538163a9a76d2fe3
Robert Colca <rco@newpharma.ro>
2018-06-06 13:43:36 +0300

    Now the algorithm finds the most used points(2)

--- Homework2/main.cpp
+++ Homework2/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <algorithm>
 #include "segment.h"
 
 using namespace std;
@@ -8,11 +10,45 @@ int main() {
             {2, 3, 4, 5, "red"},
             {1, 1, 7, 7, "blue"},
             {10, 1, 3, 4, "grey"},
-            {13, 5, 12, 10, "yellow"}
+            {13, 5, 1, 1, "yellow"}
     };
 
-    for (auto& obiect : segments) {
-        cout << obiect << endl;
+    map<pair<double, double>, int> point;
+
+    for (auto& object : segments) {
+        if (point.find(object.getFirstPointCoordinates()) != point.end()) {
+            ++point[object.getFirstPointCoordinates()];
+        } else {
+            point.insert(make_pair(object.getFirstPointCoordinates(), 1));
+        }
+
+        if (point.find(object.getSecondPointCoordinates()) != point.end()) {
+            ++point[object.getSecondPointCoordinates()];
+        } else {
+            point.insert(make_pair(object.getSecondPointCoordinates(), 1));
+        }
+
+        cout << object << endl;
+    }
+
+    map<int, pair<double, double>> sortedPoints;
+
+    for (auto el : point) {
+        sortedPoints.insert(make_pair(el.second, el.first));
+    }
+
+    cout << "The most used 2 points are: " << endl;
+    int count = 0;
+
+    map<int, pair<double, double>>::reverse_iterator it;
+
+    for (it = sortedPoints.rbegin(); it != sortedPoints.rend(); ++it) {
+        if (count == 2) {
+            break;
+        }
+
+        cout << "*(" << it->second.first << "," << it->second.second << ") (" << it->first << " usages)" << endl;
+        ++count;
     }
 
     return 0;
--- Homework2/segment.h
+++ Homework2/segment.h
@@ -9,10 +9,40 @@ using namespace std;
 
 class Segment {
     private:
-        double xA, xB, yA, yB;
+        /**
+         * @var double xA x coordinate of the point A
+         */
+        double xA;
+
+        /**
+        * @var double xB x coordinate of the point B
+        */
+        double xB;
+
+        /**
+        * @var double yA y coordinate of the point A
+        */
+        double yA;
+
+        /**
+        * @var double yB y coordinate of the point B
+        */
+        double yB;
+
+        /**
+         * @var string color Segment color
+         */
         string color;
 
     public:
+        /**
+         * Segment class constructor
+         * @param double xA
+         * @param double yA
+         * @param double xB
+         * @param double yB
+         * @param string color
+         */
         Segment(double xA, double yA, double xB, double yB, string color) {
             this->xA = xA;
             this->yA = yA;
@@ -21,10 +51,36 @@ class Segment {
             this->color = move(color);
         }
 
+        /**
+         * Gets the length of the segment
+         * @return double
+         */
         double getSegmentLength() {
             return sqrt((this->xB - this->xA) * (this->xB - this->xA) + (this->yB - this->yA) * (this->yB - this->yA));
         }
 
+        /**
+         * Gets the coordinates of the point A
+         * @return pair<double, double>
+         */
+        pair <double, double> getFirstPointCoordinates() {
+            return make_pair(this->xA, this->yA);
+        }
+
+        /**
+        * Gets the coordinates of the point B
+        * @return pair<double, double>
+        */
+        pair <double, double> getSecondPointCoordinates() {
+            return make_pair(this->xB, this->yB);
+        }
+
+        /**
+         * The << operator overloading
+         * @param &ostream os
+         * @param &obj obj
+         * @return
+         */
         friend ostream& operator << (ostream& os, Segment &obj) {
             os << "A(" << obj.xA << "," << obj.yA << ")" << endl;
             os << "B(" << obj.xB << "," << obj.yB << ")" << endl;
